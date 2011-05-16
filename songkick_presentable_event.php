c755fec3dbe988a97306053032949dfead9999d9
Sabrina Leandro <saleandro@yahoo.com>
2011-05-16 21:59:26 +0100

    Inlining date calendar style

--- songkick_presentable_event.php
+++ songkick_presentable_event.php
@@ -3,6 +3,7 @@
 class SongkickPresentableEvent {
 	function SongkickPresentableEvent($event) {
 		$this->event = $event;
+		$this->border_color = '#878787';
 	}
 	
 	function to_html($date_color) {
@@ -53,17 +54,27 @@ class SongkickPresentableEvent {
 		// Restore previous locale setting
 		setlocale(LC_TIME,$saved_locale);
 
+		// Not happy doing this, but the calendar styling is easily broken by the blog's or other plugin's styling.
+		$css = array();
+		$css['year']      = 'font-size:1.6em;line-height:1em;';
+		$css['day']       = 'display:block;font-size:1.8em;margin: 0px;margin-top: 2px;padding: 0px;';
+		$css['month']     = 'font-size:1.4em;margin: 0px;margin-bottom: 2px;padding: 0px;';
+		$css['day-month'] = 'border: 1px solid '.$this->border_color.';display:block;padding-bottom:4px;padding-top:3px;line-height:1.1em;';
+		$css['date-wrapper']   = 'font-size:7px;font-weight:bold;margin-right:10px;color:'.$this->border_color.';float:left;text-align:center;width:34px;margin-left:0px;line-height:1.1em;';
+		$css['a-date-wrapper'] = 'text-decoration: none;color:'.$this->border_color;
+		$css['day-name']       = 'background-color: #303030;color:#FFFFFF;display:block;font-size:7px;line-height:10px;padding-bottom:1px;padding-top:2px;text-shadow:1px 1px rgba(0, 0, 0, 0.6);text-transform:uppercase;';
+
 		// Construct the HTML block presenting the formatted date.
-		$override_color = (empty($date_color)) ? '' : 'style="background-color:'.$date_color.'"';
-		$str  = '<span class="date-wrapper"><a title="'.date('Y-m-d', $date).'" href="'.$this->event->uri.'">';
-		$str .= '  <span class="day-name" '.$override_color.'>'.htmlentities($day_name, ENT_QUOTES, 'UTF-8').'</span>';
-		$str .= '  <span class="day-month"><span class="month">'.htmlentities($month_name, ENT_QUOTES, 'UTF-8').'</span>';
-		$str .= '  <span class="day">'.date('d', $date).'</span></span>';
-		$str .= '  <span class="year">'.date('Y', $date).'</span>';
+		$override_color = (empty($date_color)) ? '' : ';background-color:'.$date_color;
+		$str  = '<span style="'.$css['date-wrapper'].'"><a style="'.$css['a-date-wrapper'].'" title="'.date('Y-m-d', $date).'" href="'.$this->event->uri.'">';
+		$str .= '  <span style="'.$css['day-name'].$override_color.'">'.htmlentities($day_name, ENT_QUOTES, 'UTF-8').'</span>';
+		$str .= '  <span style="'.$css['day-month'].'"><span style="'.$css['month'].'">'.htmlentities($month_name, ENT_QUOTES, 'UTF-8').'</span>';
+		$str .= '  <span style="'.$css['day'].'">'.date('d', $date).'</span></span>';
+		$str .= '  <span style="'.$css['year'].'">'.date('Y', $date).'</span>';
 		$str .= '</a></span>';
 
 		return $str;
 	}
-		
+
 }
 ?>
